49ff2877aed46fcc5ba0a27e6468c2c8271eeae5
ICS2O-1-2021 <99354489+ICS2O-1-2021@users.noreply.github.com>
2022-04-13 12:30:01 -0400

    init

---
+++ js/game.js
@@ -0,0 +1,11 @@
+/* global Phaser */
+
+const config = {
+  type: Phase,AUTO,
+  width: 1920,
+  height: 1080,
+  backgroundColor: 0x5f6e7a
+}
+
+const game = new Phase.Game(config)
+console.log(game)
