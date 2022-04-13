9c65d01ab5d2fbf57beb505d0878f5d4b268a0a8
ICS2O-1-2021 <ICS2O-1-2021@users.noreply.github.com>
2022-04-13 16:31:55 +0000

    Prettified Code!

--- js/game.js
+++ js/game.js
@@ -1,10 +1,11 @@
 /* global Phaser */
 
 const config = {
-  type: Phase,AUTO,
+  type: Phase,
+  AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  backgroundColor: 0x5f6e7a,
 }
 
 const game = new Phase.Game(config)
